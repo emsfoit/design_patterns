f67ff23da5577f138f120a8ca7d5acbe921529e2
emsfoit <emsfoit@gmail.com>
2023-11-07 11:17:11 +0100

    edit code

--- factory/example.java
+++ factory/example.java
@@ -1,3 +1,50 @@
+// Shape interface
+interface Shape {
+    void draw();
+}
+
+// Concrete implementations of Shape interface
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+// Factory class to create objects of shapes
+class ShapeFactory {
+    public Shape createShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        }
+
+        return null;
+    }
+}
+
+// Usage of the factory to create shapes
 public class Main {
     public static void main(String[] args) {
         ShapeFactory shapeFactory = new ShapeFactory();
